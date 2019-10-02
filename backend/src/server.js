340bf190fd3bbdfa56a7f14495751a9a8ba57b19
Gabarros <gabarros17@gmail.com>
2019-10-02 11:10:38 -0300

    configure nodemon

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// GET, POST, PUT, DELETE
+
 app.get('/', (req, res)=>{
-    return res.json({ message: "Hello Word" });
+    return res.json({ message: "Hello" });
 });
 app.listen(3333);

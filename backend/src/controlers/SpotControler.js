b1e77988ebcbd1a1381151dcf2ecac695df90f95
Gabarros <gabarros17@gmail.com>
2019-10-03 00:55:36 -0300

    Create Bookings

--- backend/src/controlers/SpotControler.js
+++
@@ -1,9 +0,0 @@
-const Spot = require('../models/Spot');
-
-module.exports = {
-    async store(req, res){
-        const{ filename } = req.file;
-        const{ company, techs, price } = req.body;
-        return res.json({ ok: true});
-    }
-}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,13 +3,19 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controlers/SessionController');
-const SpotController = require('./controlers/SpotControler');
-
+const SpotController = require('./controlers/SpotController');
+const DashboardController = require('./controlers/DashboardController')
+const BookingController = require('./controlers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
-routes.post('/spots',upload.single('thumbnail') ,SpotController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
+routes.get('/spots', SpotController.index);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 module.exports = routes;

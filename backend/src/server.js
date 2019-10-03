a0a345ea360f4f9dead7744f91cf5309cb022915
Gabarros <gabarros17@gmail.com>
2019-10-02 21:43:20 -0300

    Trying the post and get methods

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,26 @@ const app = express();
 
 // GET, POST, PUT, DELETE
 
-app.get('/', (req, res)=>{
-    return res.json({ message: "Hello" });
+// req.query = Acessar querys params (Para filtros)
+// req.params =  Acessar route params (Para edição)
+// req.body = Acessar corpo da requisição (Criação e edição)
+// app.get('/users/:id', (req, res)=>{
+
+//     for(let i in usuarios){
+//         if(usuarios[i].id == req.params.id){
+//         return res.json({ nome: usuarios[i].nome });
+//     }
+//     }
+// });
+
+app.use(express.json());
+app.post('/users', (req, res)=>{
+
+    return res.json(req.body);
+
 });
+
 app.listen(3333);
+
+let usuarios = [];
+usuarios.push({id: 1, nome: "Gabriel", email: "gabarros17", idade: 23});
